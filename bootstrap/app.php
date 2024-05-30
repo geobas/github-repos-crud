9975e8360b3d3012b112c4e1ae94bfddedcaf0da
geobas <ksenera@yahoo.com>
2024-05-30 14:35:39 +0300

    Exclude route from CSRF protection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             '/github/create/repository',
+            '/github/update/repository',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
